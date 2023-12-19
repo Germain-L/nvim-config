b1af029df81719da3c6f7311fbf0ff7f2eebeaed
Germain-L <germain.leignel@gmail.com>
2023-12-19 14:17:46 +0100

    rainbow csv

---
+++ lua/plugins/rainbow_csv.lua
@@ -0,0 +1,11 @@
+return {
+  "mechatroner/rainbow_csv",
+  lazy = false, -- Set this to true if you want to lazy load the plugin
+  name = "rainbow_csv",
+  priority = 9000, -- Adjust priority as needed
+  config = function()
+    -- Any additional configuration for the plugin can go here
+    -- For example, you might want to add specific settings or mappings
+  end
+}
+
