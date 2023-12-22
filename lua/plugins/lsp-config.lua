f709c40389c9ef2f7577377b540465cecaa65c91
Germain-L <germain.leignel@gmail.com>
2023-12-22 11:05:40 +0100

    lsp

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "gopls",
+          "html",
+          "htmx",
+          "angularls",
+          "ansiblels",
+          "bashls",
+          "unocss",
+          "dockerls",
+          "docker_compose_language_service",
+          "dotls",
+          "biome",
+          "kotlin_language_server",
+          "marksman",
+          "pylsp",
+          "jedi_language_server",
+          "sqlls",
+          "svelte",
+          "tailwindcss",
+          "terraformls",
+          "templ"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({})
+      lspconfig.lua_ls.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
