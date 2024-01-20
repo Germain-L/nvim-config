d5e8054cfe1c30c7f3cfa90c43fbca19bb031994
Germain-L <germain.leignel@gmail.com>
2024-01-20 11:59:41 +0100

    snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,76 +1,122 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "gopls",
-          "html",
-          "htmx",
-          "angularls",
-          "ansiblels",
-          "bashls",
-          "unocss",
-          "dockerls",
-          "docker_compose_language_service",
-          "dotls",
-          "biome",
-          "kotlin_language_server",
-          "marksman",
-          "pylsp",
-          "jedi_language_server",
-          "sqlls",
-          "svelte",
-          "tailwindcss",
-          "terraformls",
-          "templ",
-          "yamlls",
-          "rust_analyzer"
-        }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.angularls.setup({})
-      lspconfig.ansiblels.setup({})
-      lspconfig.bashls.setup({})
-      lspconfig.unocss.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.docker_compose_language_service.setup({})
-      lspconfig.dotls.setup({})
-      lspconfig.biome.setup({})
-      lspconfig.kotlin_language_server.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.pylsp.setup({})
-      lspconfig.jedi_language_server.setup({})
-      lspconfig.sqlls.setup({})
-      lspconfig.svelte.setup({})
-      lspconfig.tailwindcss.setup({})
-      lspconfig.terraformls.setup({})
-      lspconfig.templ.setup({})
-      lspconfig.yamlls.setup({})
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"gopls",
+					"html",
+					"htmx",
+					"angularls",
+					"ansiblels",
+					"bashls",
+					"unocss",
+					"dockerls",
+					"docker_compose_language_service",
+					"dotls",
+					"biome",
+					"kotlin_language_server",
+					"marksman",
+					"pylsp",
+					"jedi_language_server",
+					"sqlls",
+					"svelte",
+					"tailwindcss",
+					"terraformls",
+					"templ",
+					"yamlls",
+					"rust_analyzer",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.angularls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ansiblels.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.unocss.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.docker_compose_language_service.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.dotls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.biome.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.kotlin_language_server.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jedi_language_server.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.sqlls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.terraformls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.templ.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
